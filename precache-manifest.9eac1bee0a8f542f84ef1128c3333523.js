1716e368a4ef8476aef4dbc852605bda868d74ee
Maxime <>
2020-04-12 18:58:28 +0200

    Updates

---
+++ precache-manifest.9eac1bee0a8f542f84ef1128c3333523.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7104207ce6f121f9e95f819e3a33a3fc",
+    "url": "/signum/index.html"
+  },
+  {
+    "revision": "b7b336b970692e139e69",
+    "url": "/signum/static/css/main.ed9388d4.chunk.css"
+  },
+  {
+    "revision": "855d0a5fc3383786de00",
+    "url": "/signum/static/js/2.b6cfe872.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/signum/static/js/2.b6cfe872.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7b336b970692e139e69",
+    "url": "/signum/static/js/main.911403aa.chunk.js"
+  },
+  {
+    "revision": "ccbabd37bc1ddc23f0c8",
+    "url": "/signum/static/js/runtime-main.51092421.js"
+  }
+]);
